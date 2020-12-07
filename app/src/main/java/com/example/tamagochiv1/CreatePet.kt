65f8cbae8c5a00adfe5deacd6ff580170ded9e81
stevenwierda <49940514+stevenwierda@users.noreply.github.com>
2020-12-07 11:48:43 +0100

    skin switch
    still broken

--- app/src/main/java/com/example/tamagochiv1/CreatePet.kt
+++ app/src/main/java/com/example/tamagochiv1/CreatePet.kt
@@ -4,6 +4,7 @@ import Pet
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,11 +13,31 @@ class CreatePet: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pet_create)
+        var skin = "boxy"
         val nameBox = findViewById<EditText>(R.id.PetNameInputBox)
         val skinSelection = findViewById<SeekBar>(R.id.SkinSelect)
-        var skinNumber = skinSelection.setOnSeekBarChangeListener()
-        skinSelection.setMax()
+        val bodyPetImage = findViewById<ImageView>(R.id.BodyPetCreate)
+        skinSelection.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(skinSelection: SeekBar?, progress: Int, fromUser: Boolean) {
+             if(progress == 1){
+                bodyPetImage.setImageResource(R.drawable.petbodytransbackground)
+                skin = "boxy"
+             }
+             else if(progress == 2){
+                 bodyPetImage.setImageResource(R.drawable.bunny)
+                 skin = "bunny"
+             }
 
-        val pet = Pet(nameBox, )
+        }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+        })
+        skinSelection.setMax(2)
+        val pet = Pet(nameBox.toString(), skin)
     }
 }
