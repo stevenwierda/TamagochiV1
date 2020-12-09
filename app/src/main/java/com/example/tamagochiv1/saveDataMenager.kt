919f8ce6a5cff600829ee9e0db8fd22c3cfa85cb
stevenwierda <49940514+stevenwierda@users.noreply.github.com>
2020-12-09 11:47:25 +0100

    Create saveDataMenager.kt

---
+++ app/src/main/java/com/example/tamagochiv1/saveDataMenager.kt
@@ -0,0 +1,43 @@
+package com.example.tamagochiv1
+
+import android.content.Context
+
+class saveDataMenager(context: Context) {
+    private val saveData = context.getSharedPreferences("saveData", 0)
+
+    fun putBoolean(key: String, value: Boolean){
+        saveData.edit().putBoolean(key, value).apply()
+    }
+
+    fun getBoolean(key: String): Boolean{
+        return saveData.getBoolean(key, false)
+    }
+
+    fun putString(key: String, value: String){
+        saveData.edit().putString(key, value).apply()
+    }
+
+    fun getString(key: String): String?{
+        return saveData.getString(key, null)
+    }
+
+    fun putInt(key: String, value: Int){
+        saveData.edit().putInt(key, value).apply()
+    }
+
+    fun getInt(key: String): Int{
+        return saveData.getInt(key,0)
+    }
+
+    fun putFloat(key: String, value: Float){
+        saveData.edit().putFloat(key, value).apply()
+    }
+
+    fun getFloat(key: String): Float{
+        return saveData.getFloat(key, 0)
+    }
+
+    fun putObject(key: String, value: Object){
+
+    }
+}
