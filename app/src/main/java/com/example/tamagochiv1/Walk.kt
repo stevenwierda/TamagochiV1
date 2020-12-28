302fecfbbd9284739e688dae3abda9372318212d
stevenwierda <49940514+stevenwierda@users.noreply.github.com>
2020-12-28 15:57:59 +0100

    start of the walk mini game

--- app/src/main/java/com/example/tamagochiv1/Walk.kt
+++ app/src/main/java/com/example/tamagochiv1/Walk.kt
@@ -1,12 +1,15 @@
 package com.example.tamagochiv1
 
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.MapView
 
 class Walk : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.walk)
+        var map = findViewById<MapView>(R.id.mapView)
     }
 
 
