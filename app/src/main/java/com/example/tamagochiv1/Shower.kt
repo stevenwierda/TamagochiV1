05de604a11e1383a1d3901f5dcb57bfc7e8b5b20
stevenwierda <49940514+stevenwierda@users.noreply.github.com>
2021-01-11 16:44:23 +0100

    start of shower

--- app/src/main/java/com/example/tamagochiv1/Shower.kt
+++ app/src/main/java/com/example/tamagochiv1/Shower.kt
@@ -1,11 +1,28 @@
 package com.example.tamagochiv1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Shower: AppCompatActivity()  {
+
+    private val xDelta = 0
+    private val yDelta = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.shower)
+        setContentView(com.example.tamagochiv1.R.layout.shower)
+        val showerhead = findViewById<ImageView>(com.example.tamagochiv1.R.id.showerhead)
+        showerhead.setOnTouchListener(onTouchListener());
     }
+
+    private fun onTouchListener(): View.OnTouchListener? {
+
+    }
+
 }
