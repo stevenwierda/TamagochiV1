1a065e263e1682feb93889e516871d6eca8acec8
stevenwierda <49940514+stevenwierda@users.noreply.github.com>
2021-01-24 15:11:42 +0100

    added the needs screen + finish mic

--- app/src/main/java/com/example/tamagochiv1/MainActivity.kt
+++ app/src/main/java/com/example/tamagochiv1/MainActivity.kt
@@ -2,18 +2,26 @@ package com.example.tamagochiv1
 
 import Pet
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val saveDataMenager = SaveDataManager(this@MainActivity)
         val petAlive = saveDataMenager.getBoolean("petAlive")
-        if (petAlive){
+        if (petAlive) {
             setContentView(R.layout.activity_main)
+
+            val pet = loadData()
+            //val pet = Pet("Diezel", "jiejkl")
 
             val goToShowerButton = findViewById<ImageButton>(R.id.goToShowerButton)
             val goToWalkButton = findViewById<ImageButton>(R.id.goToWalkButton)
@@ -21,27 +29,45 @@ class MainActivity : AppCompatActivity() {
             val face = findViewById<ImageView>(R.id.face)
             val body = findViewById<ImageView>(R.id.Body)
 
-            goToShowerButton.setOnClickListener{
+            val progress_happiness = findViewById<ProgressBar>(R.id.progress_happiness)
+            val progress_hunger = findViewById<ProgressBar>(R.id.progress_hunger)
+            val progress_hygiene = findViewById<ProgressBar>(R.id.progress_hygiene)
+            val progress_energy = findViewById<ProgressBar>(R.id.progress_energy)
+            val progress_hitpoints = findViewById<ProgressBar>(R.id.progress_hitpoints)
+
+            progress_happiness.getProgressDrawable().setColorFilter(Color.parseColor("#64645c"), PorterDuff.Mode.SRC_IN)
+            progress_hunger.getProgressDrawable().setColorFilter(Color.parseColor("#64645c"), PorterDuff.Mode.SRC_IN)
+            progress_hygiene.getProgressDrawable().setColorFilter(Color.parseColor("#64645c"), PorterDuff.Mode.SRC_IN)
+            progress_energy.getProgressDrawable().setColorFilter(Color.parseColor("#64645c"), PorterDuff.Mode.SRC_IN)
+            progress_hitpoints.getProgressDrawable().setColorFilter(Color.parseColor("#64645c"), PorterDuff.Mode.SRC_IN)
+
+            progress_happiness.progress = pet.getHappyness()
+            progress_hunger.progress = pet.getHunger()
+            progress_hygiene.progress = pet.getHygiene()
+            progress_energy.progress = pet.getEnergy()
+            progress_hitpoints.progress = pet.getHitpoints()
+
+            goToShowerButton.setOnClickListener {
                 val goToShower = Intent(this, Shower::class.java)
                 startActivity(goToShower)
             }
 
-            goToWalkButton.setOnClickListener{
+            goToWalkButton.setOnClickListener {
                 val goToWalk = Intent(this, Walk::class.java)
                 startActivity(goToWalk)
             }
 
-            goToFoodButton.setOnClickListener{
+            goToFoodButton.setOnClickListener {
                 val goToFeeding = Intent(this, Feeding::class.java)
                 startActivity(goToFeeding)
             }
-        }
-        else{
+        } else {
             val goToCreatePet = Intent(this, CreatePet::class.java)
             startActivity(goToCreatePet)
         }
     }
-    fun saveData(pet:Pet){
+
+    fun saveData(pet: Pet) {
         val saveDataMenager = SaveDataManager(this@MainActivity)
         saveDataMenager.putString("name", pet.getName())
         saveDataMenager.putString("skin", pet.getSkin())
@@ -52,4 +78,19 @@ class MainActivity : AppCompatActivity() {
         saveDataMenager.putInt("hitpoints", pet.getHitpoints())
         saveDataMenager.putBoolean("petAlive", true)
     }
+
+    fun loadData(): Pet {
+        val pet = Pet("bob","test")
+        val saveDataMenager = SaveDataManager(this@MainActivity)
+        pet.setName(saveDataMenager.getString("name").toString())
+        pet.setSkin(saveDataMenager.getString("skin").toString())
+        pet.setHappyness(saveDataMenager.getInt("happyness"))
+        pet.setHunger(saveDataMenager.getInt("hunger"))
+        pet.setEnergy(saveDataMenager.getInt("energy"))
+        pet.setHygiene(saveDataMenager.getInt("hygiene"))
+        saveDataMenager.putInt("hitpoints", pet.getHitpoints())
+        pet.setHitpoints(saveDataMenager.getInt("hitpoints"))
+        return pet
+    }
+
 }
--- app/src/main/java/com/example/tamagochiv1/Shower.kt
+++ app/src/main/java/com/example/tamagochiv1/Shower.kt
@@ -1,6 +1,7 @@
 package com.example.tamagochiv1
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
@@ -14,8 +15,8 @@ import java.util.*
 
 
 class Shower: AppCompatActivity()  {
-    private var xDelta = 0
-    private var yDelta = 0
+    private var xDelta = 1958
+    private var yDelta = 719
     private var wasProgress = 0
     private var dryProgress = 0
     private val audio_recoder = audioRecorder()
@@ -28,22 +29,22 @@ class Shower: AppCompatActivity()  {
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), 7896)
         }
-        audio_recoder.start()
 
         val mainLayout = findViewById<RelativeLayout>(R.id.main)
-        val showerhead = findViewById<ImageView>(R.id.showerhead)
+        val sponge = findViewById<ImageView>(R.id.showerhead)
         val progressWassing = findViewById<ProgressBar>(R.id.progressbarWashing)
-        val cleanBut = findViewById<ImageButton>(R.id.draybutton)
+        val progressDrying = findViewById<ProgressBar>(R.id.progressbarDrying)
+        val cleanBut = findViewById<ImageView>(R.id.draybutton)
         progressWassing.progress = wasProgress
-        showerhead.setOnTouchListener(onTouchListener())
-        cleanBut.setOnClickListener {
-            Log.d("audio lvl",audio_recoder.amplitude.toString())
-        }
+        progressDrying.progress = dryProgress
+        audio_recoder.start()
+        sponge.setOnTouchListener(onTouchListener())
+        cleanBut.setOnTouchListener(onTouchListener2())
 
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private fun onTouchListener(): OnTouchListener? {
+@SuppressLint("ClickableViewAccessibility")
+    private fun onTouchListener(): OnTouchListener {
         return OnTouchListener { view, event ->
             val x = event.rawX.toInt()
             val y = event.rawY.toInt()
@@ -69,7 +70,7 @@ class Shower: AppCompatActivity()  {
             }
             if (event.rawY.toInt() in 841..1343 && event.rawX.toInt() in 287..814){
                 //progressWassing
-                if(wasProgress < 101) {
+                if(wasProgress < 100) {
                     wasProgress = wasProgress + 1
                     val progressWassing = findViewById<ProgressBar>(R.id.progressbarWashing)
                     progressWassing.progress = wasProgress
@@ -82,4 +83,54 @@ class Shower: AppCompatActivity()  {
             true
         }
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun onTouchListener2(): OnTouchListener {
+        return OnTouchListener { view, event ->
+            val x = event.rawX.toInt()
+            val y = event.rawY.toInt()
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_DOWN -> {
+                    val lParams = view.layoutParams as RelativeLayout.LayoutParams
+                    xDelta = x - lParams.leftMargin
+                    yDelta = y - lParams.topMargin
+                    //Log.d("coords",event.rawY.toString() + ":"+ event.rawX.toString())
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    val layoutParams = view.layoutParams as RelativeLayout.LayoutParams
+                    layoutParams.leftMargin = x - xDelta
+                    layoutParams.topMargin = y - yDelta
+                    layoutParams.rightMargin = 0
+                    layoutParams.bottomMargin = 0
+                    view.layoutParams = layoutParams
+
+
+                }
+            }
+            if (event.rawY.toInt() in 841..1343 && event.rawX.toInt() in 287..814 && wasProgress == 100){
+                var audioLevel = audio_recoder.amplitude
+                Log.d("audio lvl",audioLevel.toString())
+                val progressDrying = findViewById<ProgressBar>(R.id.progressbarDrying)
+                if (audioLevel > 800){
+                    dryProgress = dryProgress + 1
+                    progressDrying.progress = dryProgress
+                    if (dryProgress == 100){
+                        val saveData = getSharedPreferences("saveData", 0)
+                        var cleanness = saveData.getInt("hygiene",0)
+                        //var cleanness = saveDataMenager.getInt("hygiene")
+                        cleanness = cleanness + 100
+                        //saveDataMenager.putInt("hygiene", cleanness)
+                        saveData.edit().putInt("hygiene", cleanness).apply()
+                        val goToMain = Intent(this, MainActivity::class.java)
+                        startActivity(goToMain)
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    private class micLvlCheck()
+
 }
